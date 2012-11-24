5d94dd5f885a476f29ad180271aee7a6df6455a2
Julian Lam <julian.lam@gmail.com>
2012-11-24 10:40:20 -0500

    prettifying the newpost form

--- php/pages/newpost.php
+++ php/pages/newpost.php
@@ -1,13 +1,15 @@
 <div id='form-container'>
 						<div id='form-errors'></div>
-						<div class='code-form ui-widget'>
-							<label>Title</label><br/>
-							<input id='post-title' name='title' /><br/>
-							<label>Language</label><br/>
-							
-							<select id='post-language'>
-								
-							</select><br/>
+						<div class='code-form'>
+							<div class="inline">
+								<label>Title</label><br/>
+								<input id='post-title' name='title' />
+							</div>
+							<div class="inline">
+								<label>Language</label><br/>
+								<select id='post-language'></select>
+							</div>
+							<br />
 							<label>Code</label><br/>
 							<textarea id='post-code' name='code' ></textarea><br/>
 							
